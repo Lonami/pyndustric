dc7be2d2dbe754b80e8b678271ddf18f3d982330
Lonami Exo <totufals@hotmail.com>
2021-01-11 21:34:58 +0100

    Initial support for better labels and jumps
    Credit to @TelosTelos on 250e855f0d7cd49216aebb53821aa78eaea3ac70.

--- pyndustric/compiler.py
+++ pyndustric/compiler.py
@@ -1,5 +1,4 @@
 from .constants import *
-from collections import Counter
 from dataclasses import dataclass
 import ast
 import inspect
@@ -9,8 +8,24 @@ import textwrap
 
 @dataclass
 class Function:
-    start: int
-    argc: int
+    """
+    Stores information about a user-defined functions which are callable within a compilable program.
+    """
+
+    start: int  # line number for the start of the function within the compiled program
+    argc: int  # count of number of arguments the function takes
+
+
+class _Instruction(str):
+    """
+    Represents a mlog instruction.
+    """
+
+
+class _Label(_Instruction):
+    """
+    Represents a no-op instruction used to label certain destinations for mlog jumps.
+    """
 
 
 class CompilerError(ValueError):
@@ -49,11 +64,14 @@ def _parse_code(code: str):
 
 class Compiler(ast.NodeVisitor):
     def __init__(self):
-        self._ins = [
-            f"set {REG_STACK} 0",
-        ]
+        self._ins = [_Instruction(f"set {REG_STACK} 0")]
         self._in_def = None
         self._functions = {}
+
+    def ins_append(self, ins):
+        if not isinstance(ins, _Instruction):
+            ins = _Instruction(ins)
+        self._ins.append(ins)
 
     def compile(self, code):
         if inspect.isfunction(code):
@@ -93,7 +111,7 @@ class Compiler(ast.NodeVisitor):
 
         if len(node.targets) > 1:
             for additional_target in node.targets[1:]:
-                self._ins.append(f"set {additional_target.id} {target.id}")
+                self.ins_append(f"set {additional_target.id} {target.id}")
 
     def insert_assign_ins(self, variable: str, value: ast.AST):
         if isinstance(value, ast.BinOp):
@@ -103,7 +121,7 @@ class Compiler(ast.NodeVisitor):
 
             left = self.as_value(value.left)
             right = self.as_value(value.right)
-            self._ins.append(f"op {op} {variable} {left} {right}")
+            self.ins_append(f"op {op} {variable} {left} {right}")
 
         elif isinstance(value, ast.Compare):
             if len(value.ops) != 1 or len(value.comparators) != 1:
@@ -115,8 +133,7 @@ class Compiler(ast.NodeVisitor):
 
             left = self.as_value(value.left)
             right = self.as_value(value.comparators[0])
-            self._ins.append(f"op {cmp} {variable} {left} {right}")
-
+            self.ins_append(f"op {cmp} {variable} {left} {right}")
         elif (
             isinstance(value, ast.Call)
             and isinstance(value.func, ast.Attribute)
@@ -131,18 +148,18 @@ class Compiler(ast.NodeVisitor):
             if attr is None:
                 raise CompilerError(ERR_UNSUPPORTED_SYSCALL, value)
 
-            self._ins.append(f"sensor {variable} {arg} {attr}")
+            self.ins_append(f"sensor {variable} {arg} {attr}")
 
         elif isinstance(value, ast.Attribute):
             obj = value.value.id
             attr = value.attr
 
             mlog_attr = "@" + attr.replace("_", "-")
-            self._ins.append(f"sensor {variable} {obj} {mlog_attr}")
+            self.ins_append(f"sensor {variable} {obj} {mlog_attr}")
 
         else:
             val = self.as_value(value)
-            self._ins.append(f"set {variable} {val}")
+            self.ins_append(f"set {variable} {val}")
 
     def visit_AugAssign(self, node: ast.Assign):
         target = node.target  # e.g., x in "x += 1"
@@ -154,50 +171,95 @@ class Compiler(ast.NodeVisitor):
             raise CompilerError(ERR_UNSUPPORTED_OP, node)
 
         right = self.as_value(node.value)
-        self._ins.append(f"op {op} {target.id} {target.id} {right}")
+        self.ins_append(f"op {op} {target.id} {target.id} {right}")
+
+    def conditional_jump(self, destination_label, test, jump_if_test=True):
+        if isinstance(test, ast.Compare):
+            if len(test.ops) != 1 or len(test.comparators) != 1:
+                raise CompilerError(ERR_UNSUPPORTED_EXPR, test)
+
+            cmp = BIN_CMP.get(type(test.ops[0]))
+            left = self.as_value(test.left)
+            right = self.as_value(test.comparators[0])
+
+        elif isinstance(test, ast.BoolOp):
+            cmp = BIN_CMP.get(type(test.op))
+            left, right = self.as_value(test.values[0]), self.as_value(test.values[1])
+
+        else:
+            left, cmp, right = self.as_value(test), "notEqual", 0
+
+        if not jump_if_test:
+            cmp = NEGATED_BIN_CMP.get(cmp)
+        if cmp is None:
+            raise CompilerError(ERR_UNSUPPORTED_OP, test)
+
+        if cmp == "and":
+            failed_label = self.Label("one_is_false")
+            self.ins_append(
+                f"jump {failed_label} equal {left} 0 "
+            )
+            self.ins_append(
+                f"jump {destination_label} notEqual {right} 0 "
+            )
+            self.ins_append(failed_label)
+        elif cmp == "or":
+            self.ins_append(
+                f"jump {destination_label} notEqual {left} 0 "
+            )
+            self.ins_append(f"jump {destination_label} notEqual {right} 0 ")
+        elif cmp == "nand":
+            self.ins_append(
+                f"jump {destination_label} equal {left} 0 "
+            )
+            self.ins_append(f"jump {destination_label} equal {right} 0 ")
+        elif cmp == "nor":
+            failed_label = self.Label("one_is_true")
+            self.ins_append(
+                f"jump {failed_label} notEqual {left} 0 "
+            )
+            self.ins_append(
+                f"jump {destination_label} Equal {right} 0 "
+            )
+            self.ins_append(failed_label)
+        else:
+            self.ins_append(f"jump {destination_label} {cmp} {left} {right}")
 
     def visit_If(self, node):
-        if isinstance(node.test, ast.Compare):
-            if len(node.test.ops) != 1 or len(node.test.comparators) != 1:
-                raise CompilerError(ERR_UNSUPPORTED_EXPR)
-
-            cmp = BIN_CMP.get(type(node.test.ops[0]))
-            if cmp is None:
-                raise CompilerError(ERR_UNSUPPORTED_OP, node)
-
-            left = self.as_value(node.test.left)
-            right = self.as_value(node.test.comparators[0])
-            self._ins.append(f"jump {{}} {cmp} {left} {right}")
-        else:
-            test = self.as_value(node.test)
-            self._ins.append(f"jump {{}} notEqual {test} 0")
-
-        initial = len(self._ins) - 1
-
-        for subnode in node.orelse:
-            self.visit(subnode)
-        self._ins.append(f"jump {{}} always")
-        orelse = len(self._ins) - 1
-
+        endif_label = self.Label(
+            "endif"
+        )
+        if_false_label = (
+            self.Label("else") if node.orelse else endif_label
+        )
+        self.conditional_jump(
+            if_false_label, node.test, jump_if_test=False
+        )
         for subnode in node.body:
             self.visit(subnode)
-        end = len(self._ins)
-
-        self._ins[initial] = self._ins[initial].format(orelse + 1)
-        self._ins[orelse] = self._ins[orelse].format(end)
+        if node.orelse:
+            self.ins_append(
+                f"jump {endif_label} always"
+            )
+            self.ins_append(if_false_label)
+            for subnode in node.orelse:
+                self.visit(subnode)
+        self.ins_append(endif_label)
 
     def visit_While(self, node):
-        self._ins.append(f"jump {{}} always")
-        initial = len(self._ins) - 1
-
+        """This will be called for any* while loop."""
+        body_label = self.Label("while_body")
+        end_label = self.Label("while_end")
+        self.conditional_jump(
+            end_label, node.test, jump_if_test=False
+        )
+        self.ins_append(body_label)
         for subnode in node.body:
             self.visit(subnode)
-
-        test = self.as_value(node.test)
-        self._ins.append(f"jump {initial + 1} notEqual {test} 0")
-        end = len(self._ins) - 1
-
-        self._ins[initial] = self._ins[initial].format(end)
+        self.conditional_jump(
+            body_label, node.test, jump_if_test=True
+        )
+        self.ins_append(end_label)
 
     def visit_For(self, node):
         target = node.target
@@ -233,17 +295,17 @@ class Compiler(ast.NodeVisitor):
         else:
             raise CompilerError(ERR_UNSUPPORTED_ITER, node)
 
-        self._ins.append(f"set {it} {start}")
+        self.ins_append(f"set {it} {start}")
 
-        self._ins.append(f"jump {{}} greaterThanEq {it} {end}")
+        self.ins_append(f"jump {{}} greaterThanEq {it} {end}")
         condition = len(self._ins) - 1
 
         self._ins.extend(inject)
         for subnode in node.body:
             self.visit(subnode)
 
-        self._ins.append(f"op add {it} {it} {step}")
-        self._ins.append(f"jump {condition} always")
+        self.ins_append(f"op add {it} {it} {step}")
+        self.ins_append(f"jump {condition} always")
         self._ins[condition] = self._ins[condition].format(len(self._ins))
 
     def visit_FunctionDef(self, node):
@@ -275,15 +337,15 @@ class Compiler(ast.NodeVisitor):
                 raise CompilerError(ERR_INVALID_DEF, node)
 
         # TODO it's better to put functions at the end and not have to skip them as code, but jumps need fixing
-        self._ins.append("jump {} always")
+        self.ins_append("jump {} always")
 
-        prologue = len(self._ins)
+        prologue = len(self._ins)  # line number where the function definition begins
         self._functions[node.name] = Function(start=prologue, argc=len(args.args))
 
-        self._ins.append(f"read {reg_ret} cell1 {REG_STACK}")
+        self.ins_append(f"read {reg_ret} cell1 {REG_STACK}")
         for arg in args.args:
-            self._ins.append(f"op sub {REG_STACK} {REG_STACK} 1")
-            self._ins.append(f"read {arg.arg} cell1 {REG_STACK}")
+            self.ins_append(f"op sub {REG_STACK} {REG_STACK} 1")
+            self.ins_append(f"read {arg.arg} cell1 {REG_STACK}")
 
         for subnode in node.body:
             self.visit(subnode)
@@ -295,7 +357,7 @@ class Compiler(ast.NodeVisitor):
                 self._ins[i] = self._ins[i].format(epilogue=epilogue)
 
         # Add 1 to the return value to skip the jump that made the call.
-        self._ins.append(f"op add @counter {reg_ret} 1")
+        self.ins_append(f"op add @counter {reg_ret} 1")
 
         end = len(self._ins)
         self._ins[prologue - 1] = self._ins[prologue - 1].format(end)
@@ -303,8 +365,8 @@ class Compiler(ast.NodeVisitor):
 
     def visit_Return(self, node):
         val = self.as_value(node.value)
-        self._ins.append(f"set {REG_RET} {val}")
-        self._ins.append("jump {epilogue} always")
+        self.ins_append(f"set {REG_RET} {val}")
+        self.ins_append("jump {epilogue} always")
 
     def visit_Expr(self, node):
         call = node.value
@@ -341,23 +403,20 @@ class Compiler(ast.NodeVisitor):
                         raise CompilerError(ERR_BAD_SYSCALL_ARGS, node)
 
                     val = self.as_value(value.value)
-                    self._ins.append(f"print {val}")
+                    self.ins_append(f"print {val}")
                 elif isinstance(value, ast.Constant):
                     val = self.as_value(value)
-                    self._ins.append(f"print {val}")
+                    self.ins_append(f"print {val}")
                 else:
                     raise CompilerError(ERR_BAD_SYSCALL_ARGS, node)
         else:
             val = self.as_value(arg)
-            self._ins.append(f"print {val}")
+            self.ins_append(f"print {val}")
 
         flush = True
         for kw in node.keywords:
             if kw.arg == "flush":
-                if isinstance(kw.value, ast.Constant) and kw.value.value in (
-                    False,
-                    True,
-                ):
+                if isinstance(kw.value, ast.Constant) and kw.value.value in (False, True):
                     flush = kw.value.value
                 elif isinstance(kw.value, ast.Name):
                     flush = kw.value.id
@@ -367,9 +426,9 @@ class Compiler(ast.NodeVisitor):
                 raise CompilerError(ERR_BAD_SYSCALL_ARGS, node)
 
         if isinstance(flush, str):
-            self._ins.append(f"printflush {flush}")
+            self.ins_append(f"printflush {flush}")
         elif flush:
-            self._ins.append(f"printflush message1")
+            self.ins_append(f"printflush message1")
 
     def emit_screen_syscall(self, node: ast.Call):
         method = node.func.attr
@@ -378,7 +437,7 @@ class Compiler(ast.NodeVisitor):
                 raise CompilerError(ERR_BAD_SYSCALL_ARGS, node)
 
             r, g, b = map(self.as_value, node.args)
-            self._ins.append(f"draw clear {r} {g} {b}")
+            self.ins_append(f"draw clear {r} {g} {b}")
 
         elif method == "color":
             if len(node.args) == 3:
@@ -388,35 +447,35 @@ class Compiler(ast.NodeVisitor):
             else:
                 raise CompilerError(ERR_BAD_SYSCALL_ARGS, node)
 
-            self._ins.append(f"draw color {r} {g} {b} {a}")
+            self.ins_append(f"draw color {r} {g} {b} {a}")
 
         elif method == "stroke":
             if len(node.args) != 1:
                 raise CompilerError(ERR_BAD_SYSCALL_ARGS, node)
 
             width = self.as_value(node.args[0])
-            self._ins.append(f"draw stroke {width}")
+            self.ins_append(f"draw stroke {width}")
 
         elif method == "line":
             if len(node.args) != 4:
                 raise CompilerError(ERR_BAD_SYSCALL_ARGS, node)
 
             x0, y0, x1, y1 = map(self.as_value, node.args)
-            self._ins.append(f"draw line {x0} {y0} {x1} {y1}")
+            self.ins_append(f"draw line {x0} {y0} {x1} {y1}")
 
         elif method == "rect":
             if len(node.args) != 4:
                 raise CompilerError(ERR_BAD_SYSCALL_ARGS, node)
 
             x, y, width, height = map(self.as_value, node.args)
-            self._ins.append(f"draw rect {x} {y} {width} {height}")
+            self.ins_append(f"draw rect {x} {y} {width} {height}")
 
         elif method == "hollow_rect":
             if len(node.args) != 4:
                 raise CompilerError(ERR_BAD_SYSCALL_ARGS, node)
 
             x, y, width, height = map(self.as_value, node.args)
-            self._ins.append(f"draw lineRect {x} {y} {width} {height}")
+            self.ins_append(f"draw lineRect {x} {y} {width} {height}")
 
         elif method == "poly":
             if len(node.args) == 4:
@@ -426,7 +485,7 @@ class Compiler(ast.NodeVisitor):
             else:
                 raise CompilerError(ERR_BAD_SYSCALL_ARGS, node)
 
-            self._ins.append(f"draw poly {x} {y} {sides} {radius} {rotation}")
+            self.ins_append(f"draw poly {x} {y} {sides} {radius} {rotation}")
 
         elif method == "hollow_poly":
             if len(node.args) == 4:
@@ -436,24 +495,24 @@ class Compiler(ast.NodeVisitor):
             else:
                 raise CompilerError(ERR_BAD_SYSCALL_ARGS, node)
 
-            self._ins.append(f"draw linePoly {x} {y} {sides} {radius} {rotation}")
+            self.ins_append(f"draw linePoly {x} {y} {sides} {radius} {rotation}")
 
         elif method == "triangle":
             if len(node.args) != 6:
                 raise CompilerError(ERR_BAD_SYSCALL_ARGS, node)
 
             x0, y0, x1, y1, x2, y2 = map(self.as_value, node.args)
-            self._ins.append(f"draw triangle {x0} {y0} {x1} {y1} {x2} {y2}")
+            self.ins_append(f"draw triangle {x0} {y0} {x1} {y1} {x2} {y2}")
 
         # elif method == 'image':
         #     pass
 
         elif method == "flush":
             if len(node.args) == 0:
-                self._ins.append(f"drawflush display1")
+                self.ins_append(f"drawflush display1")
             elif len(node.args) == 1:
                 value = self.as_value(node.args[0])
-                self._ins.append(f"drawflush {value}")
+                self.ins_append(f"drawflush {value}")
             else:
                 raise CompilerError(ERR_BAD_SYSCALL_ARGS, node)
 
@@ -467,7 +526,7 @@ class Compiler(ast.NodeVisitor):
                 raise CompilerError(ERR_BAD_SYSCALL_ARGS, node)
 
             link, enabled = map(self.as_value, node.args)
-            self._ins.append(f"control enabled {link} {enabled}")
+            self.ins_append(f"control enabled {link} {enabled}")
         elif method == "shoot":
             if len(node.args) == 3:
                 link, x, y, enabled = *map(self.as_value, node.args), 1
@@ -476,13 +535,13 @@ class Compiler(ast.NodeVisitor):
             else:
                 raise CompilerError(ERR_BAD_SYSCALL_ARGS, node)
 
-            self._ins.append(f"control shoot {link} {x} {y} {enabled}")
+            self.ins_append(f"control shoot {link} {x} {y} {enabled}")
         elif method == "ceasefire":
             if len(node.args) != 1:
                 raise CompilerError(ERR_BAD_SYSCALL_ARGS, node)
 
             link = self.as_value(node.args[0])
-            self._ins.append(f"control shoot {link} 0 0 0")
+            self.ins_append(f"control shoot {link} 0 0 0")
         else:
             raise CompilerError(ERR_UNSUPPORTED_SYSCALL, node)
 
@@ -493,7 +552,9 @@ class Compiler(ast.NodeVisitor):
             elif isinstance(node.value, (int, float)):
                 return str(node.value)
             elif isinstance(node.value, str):
-                return '"' + "".join(c for c in node.value if c >= " " and c != '"') + '"'
+                return (
+                    '"' + "".join(c for c in node.value if c >= " " and c != '"') + '"'
+                )
             else:
                 raise CompilerError(ERR_COMPLEX_VALUE, node)
         elif isinstance(node, ast.Name):
@@ -505,7 +566,6 @@ class Compiler(ast.NodeVisitor):
                     return self.env_as_value(node.func)
                 else:
                     raise CompilerError(ERR_COMPLEX_VALUE, node)
-
             fn = self._functions.get(node.func.id)
             if fn is None:
                 raise CompilerError(ERR_NO_DEF, node)
@@ -515,13 +575,11 @@ class Compiler(ast.NodeVisitor):
 
             for arg in node.args:
                 val = self.as_value(arg)
-                self._ins.append(f"write {val} cell1 {REG_STACK}")
-                self._ins.append(f"op add {REG_STACK} {REG_STACK} 1")
+                self.ins_append(f"write {val} cell1 {REG_STACK}")
+                self.ins_append(f"op add {REG_STACK} {REG_STACK} 1")
 
-            # `REG_STACK` is not updated because it's immediately read by the function.
-            # If it was, the `op add` followed by the `op sub` would be redundant.
-            self._ins.append(f"write @counter cell1 {REG_STACK}")
-            self._ins.append(f"jump {fn.start} always")
+            self.ins_append(f"write @counter cell1 {REG_STACK}")
+            self.ins_append(f"jump {fn.start} always")
             return REG_RET
         else:
             raise CompilerError(ERR_COMPLEX_VALUE, node)
@@ -533,7 +591,21 @@ class Compiler(ast.NodeVisitor):
         return var
 
     def generate_masm(self):
-        if len(self._ins) + 1 > MAX_INSTRUCTIONS:
-            raise CompilerError(ERR_TOO_LONG, None)
+        n = 0
+        labels_to_linenumbers = (
+            {}
+        )
+        for i in self._ins:
+            if not i and i.label:
+                labels_to_linenumbers[i.label.name] = n
+            if i:
+                n += 1
 
-        return "\n".join(self._ins) + "\nend\n"
+        if n > MAX_INSTRUCTIONS:
+            raise CompilerError(ERR_TOO_LONG, ast.Module(lineno=0, col_offset=0))
+
+        masm_with_labels = "\n".join(i for i in self._ins if i)
+
+        masm_with_linenumbers = masm_with_labels.format_map(labels_to_linenumbers)
+
+        return masm_with_linenumbers
