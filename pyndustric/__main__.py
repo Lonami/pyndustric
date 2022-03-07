611cb6ed103fdbb1ad93612f77d5a06a4d5ca0cb
bendn <70787919+bendnuts@users.noreply.github.com>
2022-03-07 14:54:33 +0700

    Fix handling of input files (#42)

--- pyndustric/__main__.py
+++ pyndustric/__main__.py
@@ -1,28 +1,29 @@
 import argparse
+
 import pyndustric
 import sys
 import time
 
 
-def parse_args():
+def create_args():
     parser = argparse.ArgumentParser(
         description="A compiler from Python to Mindustry's assembly (logic programming language)."
     )
 
-    arg = parser.add_argument
-    arg("files", help="source .py files to compile")
-    arg(
+    parser.add_argument("files", action="store", nargs="+", type=str, help="source files to compile")
+    parser.add_argument(
         "-c",
         "--clipboard",
         action="store_true",
         help="copy the generated code to clipboard (requires `autoit`)",
     )
 
-    return parser.parse_args()
+    return parser
 
 
 def main():
-    args = parse_args()
+    parser = create_args()
+    args = parser.parse_args()
     complete_masm = ""
     for file in args.files:
         print(f"# reading {file}...", file=sys.stderr)
@@ -44,10 +45,8 @@ def main():
         )
 
     if args.clipboard:
-        import ait
-
-        ait.copy(complete_masm)
-
+            import ait
+            ait.copy(complete_masm)
 
 if __name__ == "__main__":
     main()
