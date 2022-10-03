42d8766f34ee90ef37cc2a51e30579bae115b289
Lonami Exo <totufals@hotmail.com>
2022-10-03 13:27:02 +0200

    Run black

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from distutils.core import setup
 
+
 def load_version():
-    with open('pyndustric/version.py', 'r', encoding='utf-8') as f:
+    with open("pyndustric/version.py", "r", encoding="utf-8") as f:
         g, l = {}, {}
         # This exec is fine. The installed program would execute this exact file once imported anyway.
         # A regex or other hand-rolled parsed would be error-prone and silly just to avoid the exec.
         exec(f.read(), g, l)
-    return l['__version__']
+    return l["__version__"]
 
-setup(name='pyndustric', version=load_version(), packages=['pyndustric'])
+
+setup(name="pyndustric", version=load_version(), packages=["pyndustric"])
