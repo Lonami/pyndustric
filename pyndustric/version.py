17440e08bf8f035f9d761e4d2380913fd2654fc3
Lonami Exo <totufals@hotmail.com>
2022-10-03 13:23:27 +0200

    Dynamically load version in setup.py

---
+++ pyndustric/version.py
@@ -0,0 +1,5 @@
+# This version MUST comply with PEP 440.
+# https://www.python.org/dev/peps/pep-0440/
+#
+# This file is eval'ed by setup.py so it should only contain the __version__ variable.
+__version__ = "0.1"
--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
 from distutils.core import setup
 
-setup(name='pyndustric', version='0.1', packages=['pyndustric'])
+def load_version():
+    with open('pyndustric/version.py', 'r', encoding='utf-8') as f:
+        g, l = {}, {}
+        # This exec is fine. The installed program would execute this exact file once imported anyway.
+        # A regex or other hand-rolled parsed would be error-prone and silly just to avoid the exec.
+        exec(f.read(), g, l)
+    return l['__version__']
+
+setup(name='pyndustric', version=load_version(), packages=['pyndustric'])
